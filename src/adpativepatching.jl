b03b20daff10731663589d2bd982a8743744c9bc
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2023-07-13 10:02:32 +0900

    Use default format

--- src/adpativepatching.jl
+++ src/adpativepatching.jl
@@ -1,62 +1,65 @@
 abstract type AbstractAdaptiveTCINode{C} end
 
 struct AdaptiveTCILeaf{C} <: AbstractAdaptiveTCINode{C}
-  data::C
-  prefix::Vector{Int}
+    data::C
+    prefix::Vector{Int}
 end
 
 function Base.show(io::IO, obj::AdaptiveTCILeaf{C}) where {C}
-  prefix = convert.(Int, obj.prefix)
-  return println(
-    io, "  "^length(prefix) * "Leaf $(prefix): rank=$(maximum(_linkdims(obj.data)))"
-  )
+    prefix = convert.(Int, obj.prefix)
+    return println(
+        io,
+        "  "^length(prefix) * "Leaf $(prefix): rank=$(maximum(_linkdims(obj.data)))",
+    )
 end
 
 struct AdaptiveTCIInternalNode{C} <: AbstractAdaptiveTCINode{C}
-  children::Dict{Int,AbstractAdaptiveTCINode{C}}
-  prefix::Vector{Int}
+    children::Dict{Int,AbstractAdaptiveTCINode{C}}
+    prefix::Vector{Int}
 
-  function AdaptiveTCIInternalNode{C}(
-    children::Dict{Int,AbstractAdaptiveTCINode{C}}, prefix::Vector{Int}
-  ) where {C}
-    return new{C}(children, prefix)
-  end
+    function AdaptiveTCIInternalNode{C}(
+        children::Dict{Int,AbstractAdaptiveTCINode{C}},
+        prefix::Vector{Int},
+    ) where {C}
+        return new{C}(children, prefix)
+    end
 end
 
 """
 prefix is the common prefix of all children
 """
 function AdaptiveTCIInternalNode{C}(
-  children::Vector{AbstractAdaptiveTCINode{C}}, prefix::Vector{Int}
+    children::Vector{AbstractAdaptiveTCINode{C}},
+    prefix::Vector{Int},
 ) where {C}
-  d = Dict{Int,AbstractAdaptiveTCINode{C}}()
-  for child in children
-    d[child.prefix[end]] = child
-  end
-  return AdaptiveTCIInternalNode{C}(d, prefix)
+    d = Dict{Int,AbstractAdaptiveTCINode{C}}()
+    for child in children
+        d[child.prefix[end]] = child
+    end
+    return AdaptiveTCIInternalNode{C}(d, prefix)
 end
 
 function Base.show(io::IO, obj::AdaptiveTCIInternalNode{C}) where {C}
-  println(
-    io,
-    "  "^length(obj.prefix) *
-    "InternalNode $(obj.prefix) with $(length(obj.children)) children",
-  )
-  for (k, v) in obj.children
-    Base.show(io, v)
-  end
+    println(
+        io,
+        "  "^length(obj.prefix) *
+        "InternalNode $(obj.prefix) with $(length(obj.children)) children",
+    )
+    for (k, v) in obj.children
+        Base.show(io, v)
+    end
 end
 
 """
 Evaluate the tree at given idx
 """
 function evaluate(obj::AdaptiveTCIInternalNode{C}, idx::AbstractVector{Int}) where {C}
-  child_key = idx[length(obj.prefix) + 1]
-  return evaluate(obj.children[child_key], idx)
+    child_key = idx[length(obj.prefix)+1]
+    return evaluate(obj.children[child_key], idx)
 end
 
 function evaluate(obj::AdaptiveTCILeaf{C}, idx::AbstractVector{Int}) where {C}
-  return _evaluate(obj.data, idx[(length(obj.prefix) + 1):end])
+    return _evaluate(obj.data, idx[(length(obj.prefix)+1):end])
 end
 
 _evaluate(obj::TensorCI2, idx) = TCI.evaluate(obj, idx)
@@ -65,37 +68,39 @@ _evaluate(obj::TensorCI2, idx) = TCI.evaluate(obj, idx)
 Convert a dictionary of patches to a tree
 """
 function _to_tree(
-  patches::Dict{Vector{Int},C}; nprefix=0
+    patches::Dict{Vector{Int},C};
+    nprefix = 0,
 )::AbstractAdaptiveTCINode{C} where {C}
-  length(unique(k[1:nprefix] for (k, v) in patches)) == 1 || error("Inconsistent prefixes")
+    length(unique(k[1:nprefix] for (k, v) in patches)) == 1 ||
+        error("Inconsistent prefixes")
 
-  common_prefix = first(patches)[1][1:nprefix]
+    common_prefix = first(patches)[1][1:nprefix]
 
-  # Return a leaf
-  if nprefix == length(first(patches)[1])
-    return AdaptiveTCILeaf{C}(first(patches)[2], common_prefix)
-  end
-
-  subgroups = Dict{Int,Dict{Vector{Int},C}}()
-
-  # Look at the first index after nprefix skips
-  # and group the patches by that index
-  for (k, v) in patches
-    idx = k[nprefix + 1]
-    if idx in keys(subgroups)
-      subgroups[idx][k] = v
-    else
-      subgroups[idx] = Dict{Vector{Int},C}(k => v)
+    # Return a leaf
+    if nprefix == length(first(patches)[1])
+        return AdaptiveTCILeaf{C}(first(patches)[2], common_prefix)
     end
-  end
 
-  # Recursively construct the tree
-  children = AbstractAdaptiveTCINode{C}[]
-  for (_, grp) in subgroups
-    push!(children, _to_tree(grp; nprefix=nprefix + 1))
-  end
+    subgroups = Dict{Int,Dict{Vector{Int},C}}()
 
-  return AdaptiveTCIInternalNode{C}(children, common_prefix)
+    # Look at the first index after nprefix skips
+    # and group the patches by that index
+    for (k, v) in patches
+        idx = k[nprefix+1]
+        if idx in keys(subgroups)
+            subgroups[idx][k] = v
+        else
+            subgroups[idx] = Dict{Vector{Int},C}(k => v)
+        end
+    end
+
+    # Recursively construct the tree
+    children = AbstractAdaptiveTCINode{C}[]
+    for (_, grp) in subgroups
+        push!(children, _to_tree(grp; nprefix = nprefix + 1))
+    end
+
+    return AdaptiveTCIInternalNode{C}(children, common_prefix)
 end
 
 """
@@ -105,116 +110,117 @@ TODO
 
   - Allow arbitrary order of partitioning
 """
-function adaptivetci(
-  ::Type{T},
-  f,
-  localdims::AbstractVector{Int};
-  tolerance::Float64=1e-8,
-  maxbonddim::Int=100,
-  firstpivot=ones(Int, length(localdims)),
-  sleep_time::Float64=1e-6,
-  verbosity::Int=0,
-  maxnleaves=100,
-  kwargs...,
-)::Union{AdaptiveTCILeaf{TensorCI2{T}},AdaptiveTCIInternalNode{TensorCI2{T}}} where {T}
-  R = length(localdims)
-  leaves = Dict{Vector{Int},Union{TensorCI2{T},Future}}()
-
-  # Add root node
-  firstpivot = TCI.optfirstpivot(f, localdims, firstpivot)
-  tci, ranks, errors = TCI.crossinterpolate2(
-    T,
+function adaptivepatchtci(
+    ::Type{T},
     f,
-    localdims,
-    [firstpivot];
-    tolerance=tolerance,
-    maxbonddim=maxbonddim,
-    verbosity=verbosity,
+    localdims::AbstractVector{Int};
+    tolerance::Float64 = 1e-8,
+    maxbonddim::Int = 100,
+    firstpivot = ones(Int, length(localdims)),
+    sleep_time::Float64 = 1e-6,
+    verbosity::Int = 0,
+    maxnleaves = 100,
     kwargs...,
-  )
-  leaves[[]] = tci
-  maxsamplevalue = tci.maxsamplevalue
+)::Union{AdaptiveTCILeaf{TensorCI2{T}},AdaptiveTCIInternalNode{TensorCI2{T}}} where {T}
+    R = length(localdims)
+    leaves = Dict{Vector{Int},Union{TensorCI2{T},Future}}()
 
-  while true
-    sleep(sleep_time) # Not to run the loop too quickly
+    # Add root node
+    firstpivot = TCI.optfirstpivot(f, localdims, firstpivot)
+    tci, ranks, errors = TCI.crossinterpolate2(
+        T,
+        f,
+        localdims,
+        [firstpivot];
+        tolerance = tolerance,
+        maxbonddim = maxbonddim,
+        verbosity = verbosity,
+        kwargs...,
+    )
+    leaves[[]] = tci
+    maxsamplevalue = tci.maxsamplevalue
 
-    done = true
-    for (prefix, tci) in leaves
-      if tci isa Future
-        done = false
-        if isready(tci)
-          res = try
-            fetch(tci)
-          catch ex
-            error("An exception occured: $ex")
-          end
-          if res isa RemoteException
-            error("An exception occured: $(res)")
-          end
-          tci = leaves[prefix] = res[1]
-          if verbosity > 0
-            println(
-              "Fetched, bond dimension = $(maximum(TCI.linkdims(leaves[prefix]))) $(TCI.maxbonderror(tci)) $(tci.maxsamplevalue) for $(prefix)",
-            )
-          end
-          maxsamplevalue = max(maxsamplevalue, leaves[prefix].maxsamplevalue)
-        end
-      elseif tci isa TensorCI2 &&
-        (TCI.maxbonderror(tci) > tolerance * maxsamplevalue) &&
-        length(leaves) < maxnleaves
-        done = false
-        delete!(leaves, prefix)
-        for ic in 1:localdims[length(prefix) + 1]
-          prefix_ = vcat(prefix, ic)
-          localdims_ = localdims[(length(prefix_) + 1):end]
-          f_ = x -> f(vcat(prefix_, x))
+    while true
+        sleep(sleep_time) # Not to run the loop too quickly
 
-          firstpivot_ = ones(Int, R - length(prefix_))
-          maxval = abs(f_(firstpivot_))
+        done = true
+        for (prefix, tci) in leaves
+            if tci isa Future
+                done = false
+                if isready(tci)
+                    res = try
+                        fetch(tci)
+                    catch ex
+                        error("An exception occured: $ex")
+                    end
+                    if res isa RemoteException
+                        error("An exception occured: $(res)")
+                    end
+                    tci = leaves[prefix] = res[1]
+                    if verbosity > 0
+                        println(
+                            "Fetched, bond dimension = $(maximum(TCI.linkdims(leaves[prefix]))) $(TCI.maxbonderror(tci)) $(tci.maxsamplevalue) for $(prefix)",
+                        )
+                    end
+                    maxsamplevalue = max(maxsamplevalue, leaves[prefix].maxsamplevalue)
+                end
+            elseif tci isa TensorCI2 &&
+                   (TCI.maxbonderror(tci) > tolerance * maxsamplevalue) &&
+                   length(leaves) < maxnleaves
+                done = false
+                delete!(leaves, prefix)
+                for ic = 1:localdims[length(prefix)+1]
+                    prefix_ = vcat(prefix, ic)
+                    localdims_ = localdims[(length(prefix_)+1):end]
+                    f_ = x -> f(vcat(prefix_, x))
 
-          for r in 1:10
-            firstpivot_rnd = [rand(1:localdims_[r]) for r in eachindex(localdims_)]
-            firstpivot_rnd = TCI.optfirstpivot(f_, localdims_, firstpivot_rnd)
-            if abs(f_(firstpivot_rnd)) > maxval
-              firstpivot_ = firstpivot_rnd
-              maxval = abs(f_(firstpivot_))
+                    firstpivot_ = ones(Int, R - length(prefix_))
+                    maxval = abs(f_(firstpivot_))
+
+                    for r = 1:10
+                        firstpivot_rnd =
+                            [rand(1:localdims_[r]) for r in eachindex(localdims_)]
+                        firstpivot_rnd = TCI.optfirstpivot(f_, localdims_, firstpivot_rnd)
+                        if abs(f_(firstpivot_rnd)) > maxval
+                            firstpivot_ = firstpivot_rnd
+                            maxval = abs(f_(firstpivot_))
+                        end
+                    end
+
+                    if verbosity > 0
+                        println("Interpolating $(prefix_) ...")
+                    end
+                    leaves[prefix_] = @spawnat :any TCI.crossinterpolate2(
+                        T,
+                        f_,
+                        localdims_,
+                        [firstpivot_];
+                        tolerance = tolerance * maxsamplevalue,
+                        maxbonddim = maxbonddim,
+                        verbosity = verbosity,
+                        normalizeerror = false,
+                        kwargs...,
+                    )
+                end
             end
-          end
-
-          if verbosity > 0
-            println("Interpolating $(prefix_) ...")
-          end
-          leaves[prefix_] = @spawnat :any TCI.crossinterpolate2(
-            T,
-            f_,
-            localdims_,
-            [firstpivot_];
-            tolerance=tolerance * maxsamplevalue,
-            maxbonddim=maxbonddim,
-            verbosity=verbosity,
-            normalizeerror=false,
-            kwargs...,
-          )
         end
-      end
+        if done
+            break
+        end
     end
-    if done
-      break
-    end
-  end
 
-  leaves_done = Dict{Vector{Int},TensorCI2{T}}()
-  for (k, v) in leaves
-    if v isa Future
-      error("Something got wrong. Not all leaves are fetched")
+    leaves_done = Dict{Vector{Int},TensorCI2{T}}()
+    for (k, v) in leaves
+        if v isa Future
+            error("Something got wrong. Not all leaves are fetched")
+        end
+        if TCI.maxbonderror(v) > tolerance * maxsamplevalue
+            error(
+                "TCI for k= $(k) has bond error $(TCI.maxbonderror(v)) larger than $(tolerance) * $(maxsamplevalue) = $(tolerance*maxsamplevalue)!",
+            )
+        end
+        leaves_done[k] = v
     end
-    if TCI.maxbonderror(v) > tolerance * maxsamplevalue
-      error(
-        "TCI for k= $(k) has bond error $(TCI.maxbonderror(v)) larger than $(tolerance) * $(maxsamplevalue) = $(tolerance*maxsamplevalue)!",
-      )
-    end
-    leaves_done[k] = v
-  end
 
-  return _to_tree(leaves_done)
+    return _to_tree(leaves_done)
 end
--- test/_grid.jl
+++ test/_grid.jl
@@ -5,8 +5,8 @@ abstract type Grid end
 Convert a grid index to the corresponding coordinate in the original coordinate system
 """
 function grididx_to_origcoord(g::Grid, index::NTuple{N,Int}) where {N}
-  all(1 .<= index .<= 2^g.R) || error("1 <= {index} <= 2^g.R")
-  return (index .- 1) .* grid_step(g) .+ grid_min(g)
+    all(1 .<= index .<= 2^g.R) || error("1 <= {index} <= 2^g.R")
+    return (index .- 1) .* grid_step(g) .+ grid_min(g)
 end
 
 # Backward compatibility
@@ -16,7 +16,7 @@ originalcoordinate(g::Grid, index::NTuple{N,Int}) where {N} = grididx_to_origcoo
 Convert an grid index to quantices indices
 """
 function grididx_to_quantics(g::Grid, grididx::NTuple{N,Int}) where {N}
-  return index_to_fused_quantics(grididx, g.R)
+    return index_to_fused_quantics(grididx, g.R)
 end
 
 # Backward compatibility
@@ -26,12 +26,12 @@ to_quantics(g::Grid, grididx::NTuple{N,Int}) where {N} = grididx_to_quantics(g, 
 Convert quantics indices to the original coordinate system
 """
 function quantics_to_origcoord(g::Grid, index::AbstractVector{QuanticsInd{N}}) where {N}
-  return grididx_to_origcoord(g, fused_quantics_to_index(index))
+    return grididx_to_origcoord(g, fused_quantics_to_index(index))
 end
 
 # Backward compatibility
 function originalcoordinate(g::Grid, index::AbstractVector{QuanticsInd{N}}) where {N}
-  return quantics_to_origcoord(g, index)
+    return quantics_to_origcoord(g, index)
 end
 
 @doc raw"""
@@ -43,8 +43,8 @@ inherently discrete.
 The linear size of the mesh is ``2^R``.
 """
 struct InherentDiscreteGrid{N} <: Grid
-  R::Int
-  origin::NTuple{N,Int}
+    R::Int
+    origin::NTuple{N,Int}
 end
 
 grid_min(grid::InherentDiscreteGrid) = grid.origin
@@ -54,19 +54,19 @@ grid_step(grid::InherentDiscreteGrid{N}) where {N} = ntuple(i -> 1, N)
 Create a grid for inherently discrete data
 """
 function InherentDiscreteGrid{N}(R::Int) where {N}
-  return InherentDiscreteGrid{N}(R, ntuple(i -> 1, N))
+    return InherentDiscreteGrid{N}(R, ntuple(i -> 1, N))
 end
 
 """
 Convert a coordinate in the original coordinate system to the corresponding grid index
 """
 function origcoord_to_grididx(g::InherentDiscreteGrid, coordinate::NTuple{N,Int}) where {N}
-  return coordinate .- grid_min(g) .+ 1
+    return coordinate .- grid_min(g) .+ 1
 end
 
 # Backward compatibility
 function gridpoint(g::InherentDiscreteGrid, coordinate::NTuple{N,Int}) where {N}
-  return origcoord_to_grididx(g, coordinate)
+    return origcoord_to_grididx(g, coordinate)
 end
 
 @doc raw"""
@@ -77,9 +77,9 @@ The grid contains values at specific, equally spaced points, which represent dis
 approximations of the original continuous data. 
 """
 struct DiscretizedGrid{N} <: Grid
-  R::Int
-  grid_min::NTuple{N,Float64}
-  grid_max::NTuple{N,Float64}
+    R::Int
+    grid_min::NTuple{N,Float64}
+    grid_max::NTuple{N,Float64}
 end
 
 grid_min(g::DiscretizedGrid) = g.grid_min
@@ -89,24 +89,24 @@ grid_step(g::DiscretizedGrid) = (g.grid_max .- g.grid_min) ./ (2^g.R)
 Create a discrete grid for continuous data
 """
 function DiscretizedGrid{N}(R::Int) where {N}
-  return DiscretizedGrid{N}(R, ntuple(i -> 0.0, N), ntuple(i -> 1.0, N))
+    return DiscretizedGrid{N}(R, ntuple(i -> 0.0, N), ntuple(i -> 1.0, N))
 end
 
 """
 Convert a coordinate in the original coordinate system to the corresponding grid index
 """
 function origcoord_to_grididx(g::DiscretizedGrid, coordinate::NTuple{N,Float64}) where {N}
-  all(grid_min(g) .<= coordinate .< g.grid_max) ||
-    error("Bound Error: $(coordinate), min=$(grid_min(g)), max=$(grid_max(g))")
-  return Int.(floor.(((coordinate .- grid_min(g)) ./ grid_step(g) .+ 1)))
+    all(grid_min(g) .<= coordinate .< g.grid_max) ||
+        error("Bound Error: $(coordinate), min=$(grid_min(g)), max=$(grid_max(g))")
+    return Int.(floor.(((coordinate .- grid_min(g)) ./ grid_step(g) .+ 1)))
 end
 
 function origcoord_to_grididx(g::DiscretizedGrid{1}, coordinate::Float64)
-  return origcoord_to_grididx(g, (coordinate,))[1]
+    return origcoord_to_grididx(g, (coordinate,))[1]
 end
 
 # Backward compatibility
 function gridpoint(g::DiscretizedGrid, coordinate::NTuple{N,Float64}) where {N}
-  return origcoord_to_grididx(g, coordinate)
+    return origcoord_to_grididx(g, coordinate)
 end
 gridpoint(g::DiscretizedGrid{1}, coordinate::Float64) = gridpoint(g, (coordinate,))[1]
