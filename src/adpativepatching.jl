ddff7b8f9e1074ff2c0ca3c6e9aa472acfe881a7
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2023-07-13 12:47:40 +0900

    WIP: refactoring adaptivepatchtci

--- src/adpativepatching.jl
+++ src/adpativepatching.jl
@@ -105,6 +105,106 @@ function _to_tree(
     return AdaptiveTCIInternalNode{C}(children, common_prefix)
 end
 
+
+mutable struct TCI2PatchCreator{T}
+    f::Any
+    localdims::Vector{Int}
+    rtol::Float64
+    maxbonddim::Int
+    verbosity::Int
+    tcikwargs::Dict
+    maxval::Float64
+    atol::Float64
+end
+
+
+function TCI2PatchCreator(
+    ::Type{T},
+    f,
+    localdims::AbstractVector{Int};
+    rtol::Float64 = 1e-8,
+    maxbonddim::Int = 100,
+    verbosity::Int = 0,
+    tcikwargs=Dict(),
+)::TCI2PatchCreator{T} where {T}
+    maxval, firstpivot = _estimate_maxval(f, localdims; ntry=100)
+
+    return TCI2PatchCreator{T}(
+        f,
+        localdims,
+        rtol,
+        maxbonddim,
+        verbosity,
+        tcikwargs,
+        maxval,
+        rtol * maxval
+    )
+end
+
+mutable struct TCI2PatchCreatorResult{T}
+    spwanresult::Union{Future,TensorCI2{T}}
+    creator::TCI2PatchCreator{T}
+end
+
+function _estimate_maxval(f, localdims; ntry=100)
+    pivot = fill(1, length(localdims))
+    maxval::Float64 = abs(f(pivot))
+    for i in 1:ntry
+        pivot_ = [rand(1:localdims[i]) for i in eachindex(localdims)]
+        pivot_ = TCI.optfirstpivot(f, localdims, pivot_)
+        maxval_ = abs(f(pivot_))
+        if maxval_ > maxval
+            maxval = maxval_
+            pivot .= pivot_
+        end
+    end
+    return maxval, pivot
+end
+
+function createpatch(obj::TCI2PatchCreator{T}, prefix::AbstractVector{Int}) where {T}
+    localdims_ = obj.localdims[(length(prefix)+1):end]
+    f_ = x -> obj.f(vcat(prefix, x))
+    firstpivot = TCI.optfirstpivot(f_, localdims_, fill(1, length(localdims_)))
+
+    result = @spawnat :any TCI.crossinterpolate2(
+        T,
+        f_,
+        localdims_,
+        [firstpivot];
+        tolerance = obj.atol,
+        maxbonddim = obj.maxbonddim,
+        verbosity = obj.verbosity,
+        normalizeerror = false,
+        obj.tcikwargs...,
+    )
+
+    return TCI2PatchCreatorResult{T}(result, obj)
+end
+
+_isready(obj::TCI2PatchCreatorResult)::Bool = isready(obj.spwanresult)
+
+function fetch!(res::TCI2PatchCreatorResult{T})::Nothing where {T}
+    _isready(res) || error("Not ready yet")
+
+    if res.spwanresult isa Future
+        res.spwanresult, _, _ = fetch(res.spwanresult)
+    elseif res.spwanresult isa RemoteException
+        error("An exception occured: $(res)")
+    else
+        error("Something got wrong")
+    end
+
+    return nothing
+end
+
+function isconverged(res::TCI2PatchCreatorResult{T})::Bool where {T}
+    res.spwanresult isa TensorCI2{T} || error("Not ready yet")
+
+    tci = res.spwanresult 
+    return TCI.maxbonderror(tci) < res.creator.atol
+end
+
+
 """
 Construct QTTs using adaptive partitioning of the domain.
 
@@ -219,6 +319,78 @@ function adaptivepatchtci(
         if TCI.maxbonderror(v) > tolerance * maxsamplevalue
             error(
                 "TCI for k= $(k) has bond error $(TCI.maxbonderror(v)) larger than $(tolerance) * $(maxsamplevalue) = $(tolerance*maxsamplevalue)!",
+            )
+        end
+        leaves_done[k] = v
+    end
+
+    return _to_tree(leaves_done)
+end
+
+
+function adaptivepatchtci2(
+    ::Type{T},
+    creator;
+    sleep_time::Float64 = 1e-6,
+    maxnleaves = 100,
+    verbosity = 0
+)::Union{AdaptiveTCILeaf{TensorCI2{T}},AdaptiveTCIInternalNode{TensorCI2{T}}} where {T}
+    R = length(creator.localdims)
+    leaves = Dict{Vector{Int},Union{TensorCI2{T},TCI2PatchCreatorResult{T}}}()
+    isconverged_leaves = Dict{Vector{Int},Bool}()
+
+    # Add root
+    root = createpatch(creator, Int[])
+    while !_isready(root)
+        sleep(sleep_time) # Not to run the loop too quickly
+    end
+    fetch!(root)
+    leaves[[]] = root.spwanresult
+    isconverged_leaves[[]] = isconverged(root)
+
+    while true
+        sleep(sleep_time) # Not to run the loop too quickly
+
+        done = true
+        for (prefix, leaf) in leaves
+
+            # Fetch leaf if ready
+            if leaf isa TCI2PatchCreatorResult{T}
+                done = false
+                if _isready(leaf)
+                    fetch!(leaf)
+                    leaves[prefix] = leaf.spwanresult
+                    isconverged_leaves[prefix] = isconverged(leaf)
+                end
+            end
+
+            if leaf isa TensorCI2 && !isconverged_leaves[prefix] && length(leaves) < maxnleaves
+                done = false
+                delete!(leaves, prefix)
+                delete!(isconverged_leaves, prefix)
+
+                for ic = 1:creator.localdims[length(prefix)+1]
+                    prefix_ = vcat(prefix, ic)
+                    if verbosity > 0
+                        println("Interpolating $(prefix_) ...")
+                    end
+                    leaves[prefix_] = createpatch(creator, prefix_)
+                end
+            end
+        end
+        if done
+            break
+        end
+    end
+
+    leaves_done = Dict{Vector{Int},TensorCI2{T}}()
+    for (k, v) in leaves
+        if !(v isa TensorCI2)
+            error("Something got wrong. Not all leaves are fetched")
+        end
+        if TCI.maxbonderror(v) > creator.atol
+            error(
+                "TCI for k= $(k) has bond error $(TCI.maxbonderror(v)) larger than $(creator.rtol) * $(creator.maxval) = $(creator.atol)!",
             )
         end
         leaves_done[k] = v
--- test/adaptivepatching.jl
+++ test/adaptivepatching.jl
@@ -18,6 +18,7 @@ addprocs(max(0, MAX_WORKERS - nworkers()))
 @everywhere ITensors.disable_warn_order()
 
 @testset "tci.jl" begin
+#==
     @testset "2D fermi gk" begin
         ek(kx, ky) = 2 * cos(kx) + 2 * cos(ky) - 1.0
 
@@ -45,6 +46,34 @@ addprocs(max(0, MAX_WORKERS - nworkers()))
             isapprox(FMPOC.evaluate(tree, pivot), f(pivot); atol = tol * absmaxval)
         end
     end
+==#
+
+@testset "2D fermi gk" begin
+    ek(kx, ky) = 2 * cos(kx) + 2 * cos(ky) - 1.0
+
+    function gk(kx, ky, β)
+        iv = im * π / β
+        return 1 / (iv - ek(kx, ky))
+    end
+
+    R = 20
+    grid = DiscretizedGrid{2}(R, (0.0, 0.0), (2π, 2π))
+    localdims = fill(4, R)
+
+    β = 20.0
+    f = x -> gk(originalcoordinate(grid, QuanticsInd{2}.(x))..., β)
+
+    tol = 1e-5
+    creator = FMPOC.TCI2PatchCreator(ComplexF64, f, localdims; maxbonddim = 45, rtol = tol, verbosity=1)
+
+    tree = FMPOC.adaptivepatchtci2(ComplexF64, creator)
+    @show tree
+
+    for _ = 1:100
+        pivot = rand(1:4, R)
+        isapprox(FMPOC.evaluate(tree, pivot), f(pivot); atol = creator.atol)
+    end
+end
 end
 
 nothing
