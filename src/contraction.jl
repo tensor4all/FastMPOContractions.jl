68d423b53b83864376d321c9a8b8945ec8e5a4f5
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2023-08-17 12:19:05 +0900

    Add more tests on densitymatrix

--- src/contraction.jl
+++ src/contraction.jl
@@ -1,4 +1,4 @@
-function contract_mpo_mpo(M1::MPO, M2::MPO; alg::String="densitymatrix", kwargs...)::MPO
+function contract_mpo_mpo(M1::MPO, M2::MPO; alg::String = "densitymatrix", kwargs...)::MPO
     if alg == "densitymatrix"
         return contract_densitymatrix(M1, M2; kwargs...)
     elseif alg == "fit"
--- test/densitymatrix.jl
+++ test/densitymatrix.jl
@@ -3,18 +3,80 @@ using Test
 import FastMPOContractions as FMPOC
 using ITensors
 
-@testset "MPO*MPO contraction (densitymatrix)" begin
-    R = 3
-    sites = [Index(2, "Qubit,n=$n") for n = 1:R]
-    a = replaceprime(randomMPO(sites), 0 => 1, 1 => 2)
-    b = randomMPO(sites)
-    ab_ref = contract(a, b; alg = "naive")
+function _randomMPO(sites::Vector{Vector{Index{T}}}; linkdims = 1) where {T}
+    N = length(sites)
+    links = [Index(linkdims, "Link,n=$n") for n = 1:N-1]
+    M = MPO(N)
+    M[1] = randomITensor(sites[1]..., links[1])
+    M[N] = randomITensor(links[N-1], sites[N]...)
+    for n = 2:N-1
+        M[n] = randomITensor(links[n-1], sites[n]..., links[n])
+    end
+    return M
+end
 
-    # MPO-MPO contraction
+@testset "densitymatrix (x-y-z)" begin
+    R = 3
+    sitesx = [Index(2, "Qubit,x=$n") for n = 1:R]
+    sitesy = [Index(2, "Qubit,y=$n") for n = 1:R]
+    sitesz = [Index(2, "Qubit,z=$n") for n = 1:R]
+
+    sitesa = collect(collect.(zip(sitesx, sitesy)))
+    sitesb = collect(collect.(zip(sitesy, sitesz)))
+    a = _randomMPO(sitesa)
+    b = _randomMPO(sitesb)
+    ab_ref = contract(a, b; alg = "naive")
     ab = FMPOC.contract_densitymatrix(a, b)
     @test ab_ref ≈ ab
+end
 
-    # MPO-MPS contraction
-    ab_MPS = FMPOC.contract_densitymatrix(a, MPS([b[n] for n in eachindex(b)]))
-    @test ab_ref ≈ MPO([ab_MPS[n] for n in eachindex(ab_MPS)])
+@testset "densitymatrix (xk-y-z)" begin
+    R = 3
+    sitesx = [Index(2, "Qubit,x=$n") for n = 1:R]
+    sitesk = [Index(2, "Qubit,k=$n") for n = 1:R]
+    sitesy = [Index(2, "Qubit,y=$n") for n = 1:R]
+    sitesz = [Index(2, "Qubit,z=$n") for n = 1:R]
+
+    sitesa = collect(collect.(zip(sitesx, sitesk, sitesy)))
+    sitesb = collect(collect.(zip(sitesy, sitesz)))
+    a = _randomMPO(sitesa)
+    b = _randomMPO(sitesb)
+    ab_ref = contract(a, b; alg = "naive")
+    ab = FMPOC.contract_densitymatrix(a, b)
+    @test ab_ref ≈ ab
+end
+
+@testset "densitymatrix (xk-y-zl)" begin
+    R = 3
+    sitesx = [Index(2, "Qubit,x=$n") for n = 1:R]
+    sitesk = [Index(2, "Qubit,k=$n") for n = 1:R]
+    sitesy = [Index(2, "Qubit,y=$n") for n = 1:R]
+    sitesz = [Index(2, "Qubit,z=$n") for n = 1:R]
+    sitesl = [Index(2, "Qubit,l=$n") for n = 1:R]
+
+    sitesa = collect(collect.(zip(sitesx, sitesk, sitesy)))
+    sitesb = collect(collect.(zip(sitesy, sitesz, sitesl)))
+    a = _randomMPO(sitesa)
+    b = _randomMPO(sitesb)
+    ab_ref = contract(a, b; alg = "naive")
+    ab = FMPOC.contract_densitymatrix(a, b)
+    @test ab_ref ≈ ab
+end
+
+@testset "densitymatrix (xk-ym-zl)" begin
+    R = 3
+    sitesx = [Index(2, "Qubit,x=$n") for n = 1:R]
+    sitesk = [Index(2, "Qubit,k=$n") for n = 1:R]
+    sitesy = [Index(2, "Qubit,y=$n") for n = 1:R]
+    sitesz = [Index(2, "Qubit,z=$n") for n = 1:R]
+    sitesl = [Index(2, "Qubit,l=$n") for n = 1:R]
+    sitesm = [Index(2, "Qubit,m=$n") for n = 1:R]
+
+    sitesa = collect(collect.(zip(sitesx, sitesk, sitesm, sitesy)))
+    sitesb = collect(collect.(zip(sitesy, sitesm, sitesz, sitesl)))
+    a = _randomMPO(sitesa)
+    b = _randomMPO(sitesb)
+    ab_ref = contract(a, b; alg = "naive")
+    ab = FMPOC.contract_densitymatrix(a, b)
+    @test ab_ref ≈ ab
 end
