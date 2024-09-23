db31c88deea21cdcac3cec0903f71fe2faa98245
srohshap <stefan.rohshap@tuwien.ac.at>
2024-09-23 10:52:15 +0200

    <src/add>: added naive algorithm to contract_mpo_mpo

--- src/contractMPO.jl
+++ src/contractMPO.jl
@@ -3,6 +3,8 @@ function contract_mpo_mpo(M1::MPO, M2::MPO; alg::String = "densitymatrix", kwarg
         return contract_densitymatrix(M1, M2; kwargs...)
     elseif alg == "fit"
         return contract_fit(M1, M2; kwargs...)
+    elseif alg == "naive"
+        return ITensors.contract(M1, M2; alg = "naive", kwargs...)
     else
         error("Unknown algorithm: $alg")
     end
