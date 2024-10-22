8e6a4d1d4125bd2f143a4efa99745ef03d7a9aaf
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2024-10-22 17:20:33 -0400

    Implement apply

--- src/contractMPO.jl
+++ src/contractMPO.jl
@@ -10,3 +10,27 @@ function contract_mpo_mpo(M1::MPO, M2::MPO; alg::String = "densitymatrix", kwarg
     end
 
 end
+
+function apply(A::MPO, Ψ::MPO; alg::String="fit", cutoff::Real=1e-25, kwargs...)::MPO
+    if :algorithm ∈ keys(kwargs)
+        error("keyword argument :algorithm is not allowed")
+    end
+    if alg == "densitymatrix" && cutoff <= 1e-10
+        @warn "cutoff is too small for densitymatrix algorithm. Use fit algorithm instead."
+    end
+    AΨ = replaceprime(
+        contract_mpo_mpo(A', MPO(collect(Ψ)); alg, kwargs...), 2 => 1)
+    MPO(collect(AΨ))
+end
+
+
+function apply(A::MPO, Ψ::MPS; alg::String="fit", cutoff::Real=1e-25, kwargs...)::MPS
+    if :algorithm ∈ keys(kwargs)
+        error("keyword argument :algorithm is not allowed")
+    end
+    if alg == "densitymatrix" && cutoff <= 1e-10
+        @warn "cutoff is too small for densitymatrix algorithm. Use fit algorithm instead."
+    end
+    AΨ = noprime.(contract_mpo_mpo(A, MPO(collect(Ψ)); alg, kwargs...))
+    MPS(collect(AΨ))
+end
