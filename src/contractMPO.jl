8a00e4a2ff267815da15daf13d19d1e27353bf7d
Gianluca Grosso <gianluca.grosso@campus.lmu.de>
2025-03-24 02:09:38 +0100

    Fix cutoff bug

--- src/contractMPO.jl
+++ src/contractMPO.jl
@@ -1,12 +1,12 @@
-function contract_mpo_mpo(M1::MPO, M2::MPO; alg::String = "densitymatrix", kwargs...)::MPO
+function contract_mpo_mpo(M1::MPO, M2::MPO; alg::String="densitymatrix", kwargs...)::MPO
     if alg == "densitymatrix"
         return contract_densitymatrix(M1, M2; kwargs...)
     elseif alg == "fit"
         return contract_fit(M1, M2; kwargs...)
     elseif alg == "zipup"
-        return ITensors.contract(M1, M2; alg = "zipup", kwargs...)
+        return ITensors.contract(M1, M2; alg="zipup", kwargs...)
     elseif alg == "naive"
-        return ITensors.contract(M1, M2; alg = "naive", kwargs...)
+        return ITensors.contract(M1, M2; alg="naive", kwargs...)
     else
         error("Unknown algorithm: $alg")
     end
@@ -21,7 +21,7 @@ function apply(A::MPO, Ψ::MPO; alg::String="fit", cutoff::Real=1e-25, kwargs...
         @warn "cutoff is too small for densitymatrix algorithm. Use fit algorithm instead."
     end
     AΨ = replaceprime(
-        contract_mpo_mpo(A', MPO(collect(Ψ)); alg, kwargs...), 2 => 1)
+        contract_mpo_mpo(A', MPO(collect(Ψ)); alg, cutoff, kwargs...), 2 => 1)
     MPO(collect(AΨ))
 end
 
@@ -33,6 +33,6 @@ function apply(A::MPO, Ψ::MPS; alg::String="fit", cutoff::Real=1e-25, kwargs...
     if alg == "densitymatrix" && cutoff <= 1e-10
         @warn "cutoff is too small for densitymatrix algorithm. Use fit algorithm instead."
     end
-    AΨ = noprime.(contract_mpo_mpo(A, MPO(collect(Ψ)); alg, kwargs...))
+    AΨ = noprime.(contract_mpo_mpo(A, MPO(collect(Ψ)); alg, cutoff, kwargs...))
     MPS(collect(AΨ))
 end
